cc55da9eacfc91ad9389cacf9da77a7d14343475
Austin Okezie <143610960+Austinokezie@users.noreply.github.com>
2025-08-27 13:25:25 +0100

    added the spinner component

---
+++ src/components/components.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Spinner from "../Assets/svg/Spinner.svg"
+
+export default function components() {
+  return (
+    <div>
+        <div>
+            <img src alt="Loading..." className='h-24'  ></img>
+        </div>
+    </div>
+  )
+}
